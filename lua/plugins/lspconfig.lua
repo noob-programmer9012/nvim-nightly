aaaa40fb9660f4dcd27adfffa8b7bd0b678fe569
noob-programmer9012 <parth.sclub@gmail.com>
2025-01-25 23:22:17 +0530

    removed ollama from terminal shortcuts

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -42,12 +42,13 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require('lspconfig')
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      lspconfig.lua_ls.setup({ on_attach = require("lsp-format").on_attach })
-      lspconfig.ts_ls.setup({ on_attach = require("lsp-format").on_attach })
-      lspconfig.dockerls.setup({ on_attach = require("lsp-format").on_attach })
-      lspconfig.clangd.setup({ on_attach = require("lsp-format").on_attach })
-      lspconfig.emmet_language_server.setup({ on_attach = require("lsp-format").on_attach })
+      lspconfig.lua_ls.setup({ on_attach = require("lsp-format").on_attach, capabilities = capabilities })
+      lspconfig.ts_ls.setup({ on_attach = require("lsp-format").on_attach, capabilities = capabilities })
+      lspconfig.dockerls.setup({ on_attach = require("lsp-format").on_attach, capabilities = capabilities })
+      lspconfig.clangd.setup({ on_attach = require("lsp-format").on_attach, capabilities = capabilities })
+      lspconfig.emmet_language_server.setup({ on_attach = require("lsp-format").on_attach, capabilities = capabilities })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, { silent = true })
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, { silent = true })
