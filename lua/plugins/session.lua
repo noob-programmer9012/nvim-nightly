147b933ff8cb73bad01a8d6a01628260ac3c4ebf
noob-programmer9012 <parth.sclub@gmail.com>
2025-02-10 22:34:59 +0530

    surrond shortcut list in surround.lua file

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -38,15 +38,3 @@ return {
     vim.keymap.set("n", "<leader>sd", resession.delete)
   end
 }
-
---     Old text                    Command         New text
--- --------------------------------------------------------------------------------
---     surr*ound_words             ysiw)           (surround_words)
---     *make strings               ys$"            "make strings"
---     [delete ar*ound me!]        ds]             delete around me!
---     remove <b>HTML t*ags</b>    dst             remove HTML tags
---     'change quot*es'            cs'"            "change quotes"
---     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
---     delete(functi*on calls)     dsf             function calls
---     bracket around select       Sb              (this is demo)
---     curly bracket around        S{/[               { this is demo }
